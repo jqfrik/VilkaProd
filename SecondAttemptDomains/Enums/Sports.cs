f0a70565a0494c9777f4ea084b7c661f36bdd1e6
Slava Pahotin <slava@bk.ru>
2021-10-14 09:48:48 +0300

    Убрал магические цифры в Sports.cs

--- SecondAttemptDomains/Enums/Sports.cs
+++ SecondAttemptDomains/Enums/Sports.cs
@@ -8,16 +8,16 @@ namespace SecondAttemptDomains.Enums
 {
     public enum Sports
     {
-        Football = 101,
-        Tennis = 102,
-        Hockey = 103,
-        Basketball = 104,
-        VoleyBall = 105,
-        Handball = 106,
-        TableTennis = 107,
-        Futsal = 108,
-        Badminton = 113,
-        Baseball = 116,
-        None = 0
+        Football,
+        Tennis,
+        Hockey,
+        Basketball,
+        VoleyBall,
+        Handball,
+        TableTennis,
+        Futsal,
+        Badminton,
+        Baseball,
+        None
     }
 }
